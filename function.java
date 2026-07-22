e077e885e30fb6c104d75a17fe71fbacd464694b
subashree <subashreesakthivel17@gmail.com>
2026-07-22 19:51:33 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+    public static void S(){
+        System.out.println("hi");
+        System.out.println("hello");
+    }
+    public static void main(String[]args){
+        S();
+    }
+}
