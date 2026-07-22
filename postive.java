221ab85edaf17a096fe35da87b7cbef5e99541d8
subashree <subashreesakthivel17@gmail.com>
2026-07-22 19:52:14 +0530

    Add files via upload

---
+++ postive.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    if(n>=0){
+	       System.out.println("positive"); 
+	    }
+	    else{
+		System.out.println("negative");
+	}
+}
+}
